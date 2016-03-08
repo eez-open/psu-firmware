520383e982ea327b83e1ddbe7b957d324cd56219
Martin Vladic <martin.vladic@gmail.com>
2016-03-08 11:37:08 +0100

    slider

--- eez_psu_sketch/touch.cpp
+++ eez_psu_sketch/touch.cpp
@@ -11,13 +11,25 @@ UTouch utouch(TOUCH_SCLK, TOUCH_CS, TOUCH_DIN, TOUCH_DOUT, TOUCH_IRQ);
 EventType event_type = TOUCH_NONE;
 int x;
 int y;
+int last_x;
+int last_y;
 
-#define BUF_SIZE 5
-#define TOLERANCE 5
+#ifdef EEZ_PSU_SIMULATOR
+#define ELIMINATE_NOISE 0
+#define CONF_TOUCH_INERTIA 1
+#else
+#define ELIMINATE_NOISE 1
+#define CONF_TOUCH_INERTIA 0.2
+#endif
 
-static int buf_x[BUF_SIZE];
-static int buf_y[BUF_SIZE];
-static int buf_i;
+#if ELIMINATE_NOISE
+#define TEST_BUF_SIZE 5
+#define TEST_TOLERANCE 5
+
+static int test_buf_x[TEST_BUF_SIZE];
+static int test_buf_y[TEST_BUF_SIZE];
+static int test_buf_i;
+#endif
 
 void init() {
 	utouch.InitTouch(PORTRAIT);
@@ -33,24 +45,31 @@ void tick(unsigned long tick_usec) {
 
         if (x != -1 && y != -1) {
             if (event_type == TOUCH_NONE || event_type == TOUCH_UP) {
-                buf_x[0] = touch::x;
-                buf_y[0] = touch::y;
-                buf_i = 1;
-                event_type = TOUCH_TEST;
-            } else if (event_type == TOUCH_TEST) {
-                buf_x[buf_i] = touch::x;
-                buf_y[buf_i] = touch::y;
-                if (++buf_i == BUF_SIZE) {
+#if ELIMINATE_NOISE
+                test_buf_x[0] = x;
+                test_buf_y[0] = y;
+                test_buf_i = 1;
+                event_type = TOUCH_DOWN_TEST;
+#else
+                event_type = TOUCH_DOWN;
+                last_x = x;
+                last_y = y;
+#endif
+#if ELIMINATE_NOISE
+            } else if (event_type == TOUCH_DOWN_TEST) {
+                test_buf_x[test_buf_i] = x;
+                test_buf_y[test_buf_i] = y;
+                if (++test_buf_i == TEST_BUF_SIZE) {
                     int max_count = 0;
                     int found_start = -1;
                     int found_end = -1;
-                    for (int i = 0; i < BUF_SIZE - 1 && max_count <= BUF_SIZE / 2; ++i) {
+                    for (int i = 0; i < TEST_BUF_SIZE - 1 && max_count <= TEST_BUF_SIZE / 2; ++i) {
                         int k = i;
                         int count = 0;
-                        for (int j = i + 1; j < BUF_SIZE; ++j) {
-                            long dx = abs(buf_x[j] - buf_x[k]);
-                            long dy = abs(buf_y[j] - buf_y[k]);
-                            if (dx <= TOLERANCE && dy <= TOLERANCE) {
+                        for (int j = i + 1; j < TEST_BUF_SIZE; ++j) {
+                            long dx = abs(test_buf_x[j] - test_buf_x[k]);
+                            long dy = abs(test_buf_y[j] - test_buf_y[k]);
+                            if (dx <= TEST_TOLERANCE && dy <= TEST_TOLERANCE) {
                                 ++count;
                                 k = j;
                             }
@@ -62,21 +81,36 @@ void tick(unsigned long tick_usec) {
                         }
                     }
 
-                    if (max_count > BUF_SIZE / 2) {
-                        x = buf_x[found_start];
-                        y = buf_y[found_start];
+                    if (max_count > TEST_BUF_SIZE / 2) {
+                        x = test_buf_x[found_start];
+                        y = test_buf_y[found_start];
             
                         event_type = TOUCH_DOWN;
+
+                        last_x = x;
+                        last_y = y;
                     } else {
-                        for (int i = 1; i < BUF_SIZE; ++i) {
-                            buf_x[i-1] = buf_x[i];
-                            buf_y[i-1] = buf_y[i];
+                        for (int i = 1; i < TEST_BUF_SIZE; ++i) {
+                            test_buf_x[i-1] = test_buf_x[i];
+                            test_buf_y[i-1] = test_buf_y[i];
                         }
-                        --buf_i;
+                        --test_buf_i;
                     }
                 }
-            } else if (event_type == TOUCH_DOWN) {
-                event_type = TOUCH_MOVE;
+#endif
+            } else {
+                if (event_type == TOUCH_DOWN) {
+                    event_type = TOUCH_MOVE;
+                }
+
+                int dx = x - last_x;
+                int dy = y - last_y;
+
+                x = (int)round(last_x + CONF_TOUCH_INERTIA * dx);
+                y = (int)round(last_y + CONF_TOUCH_INERTIA * dy);
+
+                last_x = x;
+                last_y = y;
             }
             return;
         }
@@ -84,7 +118,7 @@ void tick(unsigned long tick_usec) {
 
     if (event_type == TOUCH_DOWN || event_type == TOUCH_MOVE) {
         event_type = TOUCH_UP;
-    } else if (event_type == TOUCH_UP || TOUCH_TEST) {
+    } else if (event_type == TOUCH_UP || event_type == TOUCH_DOWN_TEST) {
         event_type = TOUCH_NONE;
     }
 }
--- eez_psu_sketch/touch.h
+++ eez_psu_sketch/touch.h
@@ -12,7 +12,7 @@ extern void tick(unsigned long tick_usec);
 
 enum EventType {
     TOUCH_NONE,
-    TOUCH_TEST,
+    TOUCH_DOWN_TEST,
     TOUCH_DOWN,
     TOUCH_MOVE,
     TOUCH_UP
