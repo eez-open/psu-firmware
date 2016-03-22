d8f433060be671f1b28402bec9362322f9a6e2bb
Martin Vladic <martin.vladic@gmail.com>
2016-03-22 11:46:26 +0100

    gui icons changed

--- eez_psu_sketch/gui_edit_mode_step.cpp
+++ eez_psu_sketch/gui_edit_mode_step.cpp
@@ -20,49 +20,57 @@
 #include "gui_edit_mode.h"
 #include "gui_edit_mode_step.h"
 
+#define CONF_GUI_EDIT_MODE_STEP_THRESHOLD_PX 10
+
 namespace eez {
 namespace psu {
 namespace gui {
 namespace edit_mode_step {
 
 static float step_value = 0.1f;
+
+static bool changed;
 static int start_y;
 
 void touch_down() {
     start_y = touch::y;
+    changed = false;
 }
+
 void touch_move() {
+    if (!changed) {
+        int d = start_y - touch::y;
+        if (abs(d) >= CONF_GUI_EDIT_MODE_STEP_THRESHOLD_PX) {
+            data::Value minValue = data::getMin(edit_mode::data_cursor, edit_mode::data_id);
+            float min = minValue.getFloat();
+            float max = data::getMax(edit_mode::data_cursor, edit_mode::data_id).getFloat();
+
+            float value = edit_mode::value.getFloat();
+
+            if (d > 0) {
+                value += step_value;
+                if (value > max) {
+                    value = max;
+                }
+            } else {
+                value -= step_value;
+                if (value < min) {
+                    value = min;
+                }
+            }
+
+            edit_mode::value = data::Value(value, minValue.getUnit());
+
+            if (edit_mode::is_interactive_mode) {
+                data::set(edit_mode::data_cursor, edit_mode::data_id, edit_mode::value);
+            }
+
+            changed = true;
+        }
+    }
 }
 
 void touch_up() {
-    int end_y = touch::y;
-
-    float value = edit_mode::value.getFloat();
-
-    int d = start_y - end_y;
-    if (d > 0) {
-        value += step_value;
-    }
-    else {
-        value -= step_value;
-    }
-
-    data::Value minValue = data::getMin(edit_mode::data_cursor, edit_mode::data_id);
-    float min = minValue.getFloat();
-    float max = data::getMax(edit_mode::data_cursor, edit_mode::data_id).getFloat();
-
-    if (value < min) {
-        value = min;
-    }
-    if (value > max) {
-        value = max;
-    }
-
-    edit_mode::value = data::Value(value, minValue.getUnit());
-
-    if (edit_mode::is_interactive_mode) {
-        data::set(edit_mode::data_cursor, edit_mode::data_id, edit_mode::value);
-    }
 }
 
 }
