bf7868c4a786442824da84d0cfe9a33a52fc6c45
Martin Vladic <martin.vladic@gmail.com>
2016-07-18 11:23:32 +0200

    FLT_MIN fix

--- eez_psu_sketch/temperature.cpp
+++ eez_psu_sketch/temperature.cpp
@@ -15,7 +15,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
+#include <float.h>
+
 #include "psu.h"
 #include "serial_psu.h"
 #include "ethernet.h"
