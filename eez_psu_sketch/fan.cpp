0c84fd595cf4694f310b027959905c95364cf711
Martin Vladic <martin.vladic@gmail.com>
2016-09-22 10:19:56 +0200

    fan debugging code removed

--- eez_psu_sketch/fan.cpp
+++ eez_psu_sketch/fan.cpp
@@ -230,8 +230,6 @@ void tick(unsigned long tick_usec) {
 						psu::setQuesBits(QUES_FAN, true);
 						psu::setCurrentMaxLimit(ERR_MAX_CURRENT);
 					}
-				} else {
-					DebugTraceF("T1=%f, T2=%f, T2-T1=%f", g_rpmMeasureT1, g_rpmMeasureT2, g_rpmMeasureT2 - g_rpmMeasureT1);
 				}
 			}
 		}
