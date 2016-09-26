5519762ddfe64cb61641d21eb6d1060b082318ef
Martin Vladic <martin.vladic@gmail.com>
2016-09-26 10:18:57 +0200

    fan fix

--- eez_psu_sketch/fan.cpp
+++ eez_psu_sketch/fan.cpp
@@ -84,7 +84,7 @@ void start_rpm_measure() {
 	g_rpmMeasureT2 = 0;
 
 	analogWrite(FAN_PWM, FAN_MAX_PWM);
-	delay(10);
+	delayMicroseconds(10);
 
 	attachInterrupt(g_rpmMeasureInterruptNumber, rpm_measure_interrupt_handler, CHANGE);
 
