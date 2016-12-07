2f737117a3b47c8366bf543f894db34a0ab662c2
Martin Vladic <martin.vladic@gmail.com>
2016-12-07 14:26:19 +0100

    watchdog impulse info fixed

--- eez_psu_sketch/channel.cpp
+++ eez_psu_sketch/channel.cpp
@@ -1473,7 +1473,7 @@ void Channel::testPwrgood(uint8_t gpio) {
     if (!(gpio & (1 << IOExpander::IO_BIT_IN_PWRGOOD))) {
         DebugTraceF("Ch%d PWRGOOD bit changed to 0", index);
 #if EEZ_PSU_SELECTED_REVISION == EEZ_PSU_REVISION_R3B4 && OPTION_WATCHDOG
-        DebugTraceF("Last watchdog impuls was before %u uS", watchdog::g_lastWatchdogImpulseTime - micros());
+        DebugTraceF("Last watchdog impuls was before %u uS", micros() - watchdog::g_lastWatchdogImpulseTime);
 #endif
         flags.powerOk = 0;
         psu::generateError(SCPI_ERROR_CH1_FAULT_DETECTED - (index - 1));
