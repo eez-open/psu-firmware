b25d49a5420a5cfbf59f2faa520831a8111bf6db
Martin Vladic <martin.vladic@gmail.com>
2017-03-10 09:51:59 +0100

    fixed: ADC reading stops after some time

--- eez_psu_sketch/adc.cpp
+++ eez_psu_sketch/adc.cpp
@@ -186,7 +186,7 @@ void AnalogDigitalConverter::tick(uint32_t tick_usec) {
         start_time = micros();
     }
 #else
-    if (start_reg0 && tick_usec > start_time + ADC_READ_TIME_US) {
+    if (start_reg0 && tick_usec - start_time > ADC_READ_TIME_US) {
         int16_t adc_data = read();
         channel.eventAdcData(adc_data);
 
